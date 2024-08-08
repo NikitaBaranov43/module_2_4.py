2b90e16ef435409c357211f849a1670171996466
NikitaBaranov43 <fast.flou@gmail.com>
2024-08-08 11:02:09 +0300

    Add files via upload

--- module_2_4.py
+++ module_2_4.py
@@ -1,24 +1,19 @@
 numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
 primes = []
 not_primes = []
+prime = 0
 
 for i in numbers:
-    is_primes = False
-    count = 0
+    if i == 1:
+        continue
     for j in numbers:
-        deli = i % j
-        if deli == 0:
-            count += 1
-        elif count == 2:
-            is_primes = True
-            continue
-        elif count == 3:
-            is_primes = False
-            break
-        if is_primes:
-            primes.append(i)
-        elif i > 1:
-            not_primes.append(i)
+        if i % j == 0:
+            prime += 1
+    if prime == 2:
+        primes.append(i)
+    else:
+        not_primes.append(i)
+    prime = 0
 print(primes)
 print(not_primes)
 
